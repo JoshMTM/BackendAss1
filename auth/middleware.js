ba7d3d2a7f3f567502f9923ee6caf52f627aa5c4
JoshMTM <joshmitima08@yahoo.com>
2021-09-17 15:00:09 +0200

    signin and middlewares

---
+++ auth/middleware.js
@@ -0,0 +1,11 @@
+const User = require("../models").user;
+const { toData } = require("./jwt");
+
+async function auth() {
+  try {
+  } catch (e) {
+    console.log(e.message);
+  }
+}
+
+module.exports = auth;
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const jsonParser = express.json();
 const PORT = 4000;
 const Teams = require("./models/team");
 const Players = require("./models/player");
+const { Op } = require("sequelize");
 
 app.use(jsonParser);
 
@@ -24,22 +25,58 @@ app.get("/teams", async (req, res, next) => {
 
 // Create a new player
 app.post("/players", async (req, res, next) => {
-  try {
+  const { name, nationality } = req.body;
+  if (!name || !nationality) {
+    res.status(400).send("missing parameters");
+  } else {
     const player = await Players.create(req.body);
     res.send(player);
+    next(e);
+  }
+});
+
+app.get("/teams/:id/players", async (req, res, next) => {
+  try {
+    const teamId = req.params.id;
+    const team = await Teams.findByPk(teamId);
+    if (!team) {
+      res.status(404).send("Non-existing team");
+    } else {
+      const players = await Players.findAll(req.body);
+      res.send(team, { include: players });
+    }
   } catch (e) {
     console.log(e.message);
     next(e);
   }
 });
 
-// const imageRouter = require("./routers/image");
-// const userRouter = require("./routers/user");
+app.put("/teams/:id", async (req, res, next) => {
+  try {
+    const teamId = parseInt(req.params.id);
+    const team = await Teams.findByPk(teamId);
+    if (!team) {
+      res.status(404).send("Team not found");
+    } else {
+      const teamUpdated = await team.update(req.body);
+      res.send(teamUpdated);
+    }
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
+});
 
-// const authRouter = require("./routers/auth");
+//Get Players by age
 
-// app.use("/users", userRouter);
-// app.use("/image", imageRouter);
-// app.use(authRouter);
+app.get("/payers/:age/", async (req, res, next) => {
+  try {
+    const age = parseInt(req.params.age);
+    const player = await Players.findAll({ where: { age: { [Op.gte]: age } } });
+    res.send(player);
+  } catch (e) {
+    next(e);
+  }
+});
 
 app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
